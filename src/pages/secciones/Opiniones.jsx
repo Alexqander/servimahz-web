a36367798f54414bcd07dffd5e0c95fd801de777
Alexander Machado <84141125+Alexqander@users.noreply.github.com>
2023-04-27 19:28:00 -0600

    [UPDATE] configuracion global

--- src/pages/secciones/Opiniones.jsx
+++ src/pages/secciones/Opiniones.jsx
@@ -1,12 +1,9 @@
 import GooglePlaceReviews from '../../components/GooglePlaceReviews '
 
 export const Opiniones = () => {
-	const apiKey = 'AIzaSyA96w5lxQO9IhgrGcF9pXPfDM-DMBRMH_Y'
-	const placeId = 'ChIJk8y4Z_N1zoURLpVHrYIFGvA'
-
 	return (
 		<>
-			<GooglePlaceReviews apiKey={apiKey} placeId={placeId} />
+			<GooglePlaceReviews />
 		</>
 	)
 }
